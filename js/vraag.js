e87f9a5bd71c84ed512c9541adc42d3eaf200997
fsahin03 <302353664@student.rocmondriaan.nl>
2021-06-22 16:13:43 +0200

    Vragen zijn gemaakt

--- js/vraag.js
+++ js/vraag.js
@@ -1,2 +1,50 @@
 let vraagje = document.getElementById('vraag');
-vraagje.innerHTML= "1. Wat is een lidwoord?";
+let buttonA1 = document.getElementById('buttonA');
+let buttonB2 = document.getElementById('buttonB');
+let buttonC3 = document.getElementById('buttonC');
+let buttonD4 = document.getElementById('buttonD');
+let juisteAntwoord = "B";
+let vraag = 1;
+
+
+vraagje.innerHTML= " 1. Wat is een lidwoord?";
+buttonA1.innerHTML = "A.één";
+buttonB2.innerHTML = "B.het";
+buttonC3.innerHTML = "C.word";
+buttonD4.innerHTML = "D.die";
+
+
+
+
+function checkAnswer (gekozenAntwoord)
+ {
+    alert(gekozenAntwoord);
+    if(juisteAntwoord == gekozenAntwoord)
+     {
+    // alert("Dit is het juiste antwoord");
+    vraag++;
+    }
+    else{
+        vraag++;
+        // alert("Dit is niet het juiste antwoord");
+    };
+        // alert("Helaas dat is niet het goede antwoord")
+        // vraagje.innerHTML = "2.Wat is de hoofdpersoon?"
+        // buttonA1.innerHTML = "A. De minst belangrijke persoon in de tekst";
+        // buttonB2.innerHTML = "B. De belangrijkste persoon in een tekst";
+        // buttonC3.innerHTML = "C. De oudste persoon in een tekst";
+        // buttonD4.innerHTML = "";
+}
+
+buttonA1.addEventListener('click',() => {
+    checkAnswer("A");
+});
+buttonB2.addEventListener('click',() => {
+    checkAnswer("B");
+});
+buttonC3.addEventListener('click',() => {
+    checkAnswer("C");
+});
+buttonD4.addEventListener('click',() => {
+    checkAnswer("D");
+});
--- vragen.md
+++ vragen.md
@@ -46,3 +46,79 @@ C.integer (int)
 D.double
 
 Vraag5
+Hoe start je een html bestand?
+A.DOCtype HTML
+B.!DOCTYPE html
+C.header
+D.doctype HTML.
+
+
+Vraag1
+Wat is een verb?
+A. werkwoord
+B. bijvoeglijk naamwoord
+C. persoonsvorm
+D. onderwerp
+
+Vraag2
+Wat is de present simple?
+A. verleden tijd
+B. enkelvoud
+C. tegenwoordige tijd
+D. toekomt
+
+Vraag3
+Wat is het hele werkwoord van to lie?
+A.lies
+B.lie
+C.lieing
+D. lying
+
+Vraag4
+Wat is de beleefde manier om te groeten?
+A. Hello how are you
+B. Yo what's poppin
+C. Good day sir
+D. wassup
+
+Vraag5
+Wat is de verleden tijd van leave?
+A. leaved
+B. left
+C. leaves
+D. leavingd
+
+
+Vraag1:
+5*(3+3*4)=
+A.120
+B.75
+C.72
+D.26
+
+Vraag2:
+12*8-6*8=
+A.48
+B.192
+C.720
+D.-480
+
+Vraag3:
+5*(12–8:4)
+A.5
+B.13
+C.50
+
+Vraag4:
+(30:2*5)−(5:2*10)
+A.50
+B.-22
+C.74,75
+D. 2,75
+
+Vraag5:
+18+6*20:(13-7) =
+A.20,2
+B.114.4
+C.38
+D.34
